5dfbdc9efe2bafd30f49939f31f1b5df5a97cdf3
rsantos88 <rasantos@it.uc3m.es>
2017-03-15 17:07:22 +0100

    added example cpp

---
+++ example/cpp/espeakExample/build/CMakeFiles/espeakExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/espeakExample.dir/EspeakExample.cpp.o"
+  "CMakeFiles/espeakExample.dir/main.cpp.o"
+  "espeakExample.pdb"
+  "espeakExample"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/espeakExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
