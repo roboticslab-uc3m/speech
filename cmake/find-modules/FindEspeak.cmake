99dfb0ede3284a0f3306727668d13480fda44bca
Bartek Łukawski <bwmn.peter@gmail.com>
2018-03-09 21:43:14 +0100

    Handle standard vars in FindEspeak.cmake

--- cmake/find-modules/FindEspeak.cmake
+++ cmake/find-modules/FindEspeak.cmake
@@ -3,21 +3,23 @@
 #  ESPEAK_FOUND - System has ESPEAK
 #  ESPEAK_INCLUDE_DIRS - The ESPEAK include directories
 #  ESPEAK_LIBRARIES - The libraries needed to use ESPEAK
-#  ESPEAK_DEFINITIONS - Compiler switches required for using ESPEAK
 
 #set(ESPEAK_DEFINITIONS ${PC_LIBXML_CFLAGS_OTHER})
 
-find_path(ESPEAK_INCLUDE_DIRS speak_lib.h
+find_path(ESPEAK_INCLUDE_DIR speak_lib.h
   $ENV{ESPEAK_DIR}
   $ENV{FFMPEG_DIR}/espeak
   /usr/local/include/espeak
   /usr/include/espeak
 )
 
-find_library(ESPEAK_LIBRARIES NAMES espeak libespeak )
+find_library(ESPEAK_LIBRARY NAMES espeak libespeak)
 
-mark_as_advanced(ESPEAK_INCLUDE_DIRS ESPEAK_LIBRARY )
+include(FindPackageHandleStandardArgs)
 
-set(ESPEAK_INCLUDE_DIRS ${ESPEAK_INCLUDE_DIR} )
-set(ESPEAK_LIBRARIES ${ESPEAK_LIBRARY} )
+find_package_handle_standard_args(Espeak DEFAULT_MSG ESPEAK_INCLUDE_DIR ESPEAK_LIBRARY)
 
+mark_as_advanced(ESPEAK_INCLUDE_DIR ESPEAK_LIBRARY)
+
+set(ESPEAK_INCLUDE_DIRS ${ESPEAK_INCLUDE_DIR})
+set(ESPEAK_LIBRARIES ${ESPEAK_LIBRARY})
