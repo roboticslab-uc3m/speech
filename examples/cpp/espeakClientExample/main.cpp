304beb66baeed75f33e01edebb35dcdfc8bbf3f3
Bartek Łukawski <bwmn.peter@gmail.com>
2021-11-11 14:40:32 +0100

    Create espeakClientExample
    Closes https://github.com/roboticslab-uc3m/speech/issues/9.

---
+++ examples/cpp/espeakClientExample/main.cpp
@@ -0,0 +1,68 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/**
+ * @ingroup teo_examples_cpp
+ * @defgroup espeakClientExample espeakClientExample
+ */
+
+#include <yarp/os/LogStream.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/Property.h>
+#include <yarp/os/RpcClient.h>
+#include <yarp/os/SystemClock.h>
+
+#include <Speech_IDL.h>
+
+constexpr auto DEFAULT_REMOTE_PORT = "/tts";
+
+int main(int argc, char * argv[])
+{
+    yarp::os::Property config;
+    config.fromCommand(argc, argv);
+
+    auto remote = config.check("remote", yarp::os::Value(DEFAULT_REMOTE_PORT), "remote server port").asString();
+
+    yarp::os::Network yarp;
+
+    if (!yarp::os::Network::checkNetwork())
+    {
+        yError() << "Please start a yarp name server first";
+        return 1;
+    }
+
+    yarp::os::RpcClient client;
+
+    if (!client.open("/espeakClientExample/rpc:s"))
+    {
+        yError() << "Unable to open client port" << client.getName();
+        return 1;
+    }
+
+    if (!yarp::os::Network::connect(client.getName(), remote + "/rpc:s"))
+    {
+        yError() << "Unable to connect to remote server port" << remote;
+        return 1;
+    }
+
+    Speech_IDL espeak;
+    espeak.yarp().attachAsClient(client);
+
+    yInfo() << "Connected to remote Espeak server";
+
+    espeak.setLanguage("mb-en1");
+    espeak.setSpeed(150); // Values 80 to 450.
+    espeak.setPitch(60); // 50 = normal
+
+    yInfo() << "Using speed" << espeak.getSpeed();
+    yInfo() << "Using pitch" << espeak.getPitch();
+
+    espeak.say("Hello, my name is Teo. I want to follow you. Please, tell me. Ok, I will follow you. Ok, I will stop following you.");
+
+    do
+    {
+        yarp::os::SystemClock::delaySystem(0.1);
+    }
+    while (!espeak.checkSayDone());
+
+    return 0;
+}
