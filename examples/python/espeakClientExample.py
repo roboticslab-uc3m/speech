cca3591ed0a060dc74a5573c57574101efe6fd77
Bartek Łukawski <bwmn.peter@gmail.com>
2021-11-12 21:36:45 +0100

    Create espeakClientExample.py

---
+++ examples/python/espeakClientExample.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import argparse
+import yarp
+import roboticslab_speech as speech
+import time
+
+parser = argparse.ArgumentParser(description='create a client connection with a remote Espeak service')
+parser.add_argument('--remote', default='/tts', type=str, help='remote server port')
+args = parser.parse_args()
+
+yarp.Network.init()
+
+if not yarp.Network.checkNetwork():
+    print('Please start a yarp name server first')
+    raise SystemExit
+
+client = yarp.RpcClient()
+
+if not client.open('/tts/client/rpc:c'):
+    print('Unable to open client port %s' % client.getName())
+    raise SystemExit
+
+if not yarp.Network.connect(client.getName(), args.remote + '/rpc:s'):
+    print('Unable to connect to remote server port %s' % args.remote)
+    raise SystemExit
+
+espeak = speech.SpeechIDL()
+espeak.yarp().attachAsClient(client)
+
+espeak.setLanguage('mb-en1')
+espeak.setSpeed(150); # Values 80 to 450.
+espeak.setPitch(60); # 50 = normal
+
+print('Using speed %d' % espeak.getSpeed())
+print('Using pitch %d' % espeak.getPitch())
+
+espeak.say('Hello, my name is Teo. I want to follow you. Please, tell me. Ok, I will follow you. Ok, I will stop following you.')
+
+while not espeak.checkSayDone():
+    time.sleep(0.1)
