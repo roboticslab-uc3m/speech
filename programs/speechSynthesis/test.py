2e7102d99bf847d28fe82cf316566e852bf3dcfc
Bartek Łukawski <bwmn.peter@gmail.com>
2023-12-11 19:56:05 +0100

    [WIP] Use `write` instead of callbacks

--- programs/speechSynthesis/test.py
+++ programs/speechSynthesis/test.py
@@ -4,60 +4,46 @@ import argparse
 import queue
 import roboticslab_speech
 import sounddevice as sd
+import threading
 import yarp
 
 from abc import ABC, abstractmethod
 from piper import PiperVoice # TODO: hide import
 from piper.download import find_voice # TODO: hide import
 
-class SynthesizerFactory(ABC):
+class SpeechSynthesizer(roboticslab_speech.SpeechSynthesis):
+    def __init__(self):
+        super().__init__()
+        self.event = threading.Event()
+        self._generator = None
+
     @abstractmethod
-    def create(self, stream, callback):
+    def get_sample_rate(self):
         pass
 
-class PiperSynthesizerFactory(SynthesizerFactory):
-    def __init__(self, device, model, rf):
-        self.device = device
-        self.model = model
-        self.rf = rf
-
-    def create(self, stream, callback):
-        return PiperSynthesizer(stream, callback, self.device, self.model, self.rf)
-
-class SpeechSynthesizer(roboticslab_speech.SpeechSynthesis):
-    def __init__(self, stream, callback, device):
-        super().__init__()
-        self.stream = stream
-        self.callback = callback
-        device_info = sd.query_devices(device, 'input')
-        # soundfile expects an int, sounddevice provides a float:
-        self.sample_rate = int(device_info['default_samplerate'])
-        self.text_queue = queue.Queue(maxsize=1)
-
     def say(self, text):
-        self.callback.set_generator(self._get_generator(text))
+        self._generator = self._make_generator(text)
+        self.event.set()
         return True
 
     @abstractmethod
-    def synthesize(self, text):
+    def _make_generator(self, text):
         pass
 
-    @abstractmethod
-    def _get_generator(self, text):
-        pass
-
-    @abstractmethod
-    def sd_callback(self, outdata, frames, time, status):
-        pass
+    def get_generator(self):
+        return self._generator
 
 class PiperSynthesizer(SpeechSynthesizer):
-    def __init__(self, stream, callback, device, model, rf):
-        super().__init__(stream, callback, device)
+    def __init__(self, model, rf):
+        super().__init__()
         self.model = model
         self.rf = rf
         self.voice = PiperVoice.load(self.model, use_cuda=False) # TODO: cuda
 
-    def _get_generator(self, text):
+    def get_sample_rate(self):
+        return self.voice.config.sample_rate
+
+    def _make_generator(self, text):
         return self.voice.synthesize_stream_raw(text)
 
     def play(self):
@@ -132,7 +118,7 @@ if args.backend == 'piper':
         print('Model must be specified for Piper')
         raise SystemExit
 
-    synthesizer_factory = PiperSynthesizerFactory(args.device, args.model, rf)
+    synthesizer = PiperSynthesizer(args.model, rf)
 else:
     print('Backend not available, must be one of: %s' % ', '.join(BACKENDS))
     raise SystemExit
@@ -147,59 +133,25 @@ if not rpc.open(args.prefix + '/rpc:s'):
     print('Unable to open RPC port')
     raise SystemExit
 
-class Callback:
-    def __init__(self):
-        self._generator = None
-        self._queued_generator = None
-
-    def set_generator(self, generator):
-        self._generator = generator
-
-    def callback(self, outdata, frames, time, status):
-        # https://stackoverflow.com/a/62609827
-
-        if self._generator is not None:
-            try:
-                raw = next(self._generator)
-
-                print('len(raw): %d, len(outdata): %d' % (len(raw), len(outdata)))
-
-                if len(outdata) > len(raw):
-                    outdata[:len(raw)] = raw
-                    outdata[len(raw):] = b'\x00' * (len(outdata) - len(raw))
-                    print('a')
-                elif len(outdata) < len(raw):
-                    outdata[:] = raw[:len(outdata)]
-                    self._queued_generator = self._generator
-                    self._generator = iter([raw[len(outdata):]])
-                    print('b')
-                else:
-                    outdata[:] = raw
-                    print('c')
-
-                return
-            except StopIteration:
-                if self._queued_generator is not None:
-                    self._generator = self._queued_generator
-                    self._queued_generator = None
-
-        outdata[:] = b'\x00' * len(outdata)
-
 try:
-    cb = Callback()
-
-    with sd.RawOutputStream(samplerate=22050,
-                            blocksize=2048,
+    with sd.RawOutputStream(samplerate=synthesizer.get_sample_rate(),
+                            blocksize=1024,
                             device=args.device,
                             dtype='int16',
-                            channels=1,
-                            callback=cb.callback) as stream:
-        synthesizer = synthesizer_factory.create(stream, cb)
+                            channels=1) as stream:
         synthesizer.yarp().attachAsServer(rpc)
 
         while True:
-            import time
-            time.sleep(0.1)
+            print('Waiting for text to synthesize...')
+            synthesizer.event.wait()
+            print('Synthesizing...')
+
+            for frames in synthesizer.get_generator():
+                print(stream.write_available)
+                stream.write(frames)
+
+            print('Done synthesizing')
+            synthesizer.event.clear()
 except KeyboardInterrupt:
     rpc.interrupt()
     rpc.close()
